68e11b00d39954ff4e5114e83ccb3d49ac1adb35
grandpharaon <81425649+grandpharaon@users.noreply.github.com>
2022-02-15 00:41:22 -0800

    Added decorations for the Calculator class files

---
+++ server/Calculator.h
@@ -0,0 +1,18 @@
+#ifndef CPSC5042CLIENTSERVER_CALCULATOR_H
+#define CPSC5042CLIENTSERVER_CALCULATOR_H
+
+#include <string>
+
+using namespace std;
+
+class Calculator
+{
+public:
+    string calculateExpression(const string& inExpr);
+
+private:
+
+};
+
+
+#endif //CPSC5042CLIENTSERVER_CALCULATOR_H
